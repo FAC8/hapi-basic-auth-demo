39fdfe5157845163c17a3895e371f2c8d443e900
mcelearr <rorymcelearney@gmail.com>
2016-07-19 08:52:56 +0100

    sets up endpoints and updates readme

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # hapi-basic-auth-demo
-A demo app using Hapi Basic Auth based on official Hapi Auth documentation
+
+## School website example
+You have been asked to set up a website for a school that will serve visitors, students and teachers. The site will contain some confidential information which you wish to keep secret. Visitors should not be able to see the students' or teachers' information, students should be able to see their own information and teachers should be able to see their own and students' information.
+
+Hapi Basic Auth provides a quick way of setting up authentication with different levels of authorization designated for certain endpoints. In your website there are three endpoints with three different security levels, or 'auth strategies':
+
+|Strategy|Endpoint  |File served |Validation function|
+|---     |---       |---         |---                |
+| -      | /        |index.html  |-                   |
+| user   | /student |student.html|validateAsUser     |
+| admin  | /teacher |teacher.html|validateAsAdmin    |
+
+Have a look through the code to understand how the authentication strategies are configured and applied. Also used in the example code are [Bell](https://www.npmjs.com/package/bell), a module for interacting with the 3rd party authentication of some popular websites such as facebook and twitter, and [hapi-auth-cookie](https://github.com/hapijs/hapi-auth-cookie) which uses authentication.
+
+## Summary
+
+Hapi is all about configuration. The beauty of using these auth plug-ins is that the auth strategies for different endpoints are defined using a config object when `server.route()` is called. It makes seeing which strategies are applied to which endpoints very readable, a massive benefit for reviewing others' code or coming back to one of your own projects.
--- server.js
+++ server.js
@@ -7,9 +7,18 @@ const Bcrypt = require('bcryptjs');
 // Basic Authentication Scheme for Hapi (https://github.com/hapijs/hapi-auth-basic)
 const Basic = require('hapi-auth-basic');
 
-const Path = require('path');
+// Bell is a module for interacting with the 3rd party authentication of some popular
+// websites such as facebook and twitter
+const Bell = require('bell')
 
-// Create server and start a connection
+// Auth cookie module for Hapi
+const AuthCookie = require('hapi-auth-cookie');
+
+// Path and Inert are used to serve up static files
+const Path = require('path');
+const Inert = require('inert');
+
+// Create server and start a connection with all paths relative to the public folder
 const server = new Hapi.Server({
     connections: {
         routes: {
@@ -19,10 +28,6 @@ const server = new Hapi.Server({
         }
     }
 });
-
-const Bell = require('bell')
-
-var AuthCookie = require('hapi-auth-cookie');
 
 server.connection({ port: process.env.PORT || 3000 });
 
@@ -82,7 +87,7 @@ const validateAsAdmin = (request, username, password, callback) => {
 };
 
 // Register basic auth scheme with Hapi
-server.register([Basic,Bell, AuthCookie], err => {
+server.register([Inert, Basic, Bell, AuthCookie], err => {
   if (err) throw err;
 
   // Create user strategy - by supplying the validateAsUser function
@@ -112,11 +117,11 @@ server.register([Basic,Bell, AuthCookie], err => {
   // Create a route with user level authentication
   server.route({
     method: 'GET',
-    path: '/authenticated',
+    path: '/student',
     config: {
       auth: 'user',
       handler(request, reply) {
-        reply(`hello ${request.auth.credentials.name}`);
+        reply.file('student.html');
       }
     }
   });
@@ -128,7 +133,7 @@ server.register([Basic,Bell, AuthCookie], err => {
     config: {
       auth: 'admin',
       handler(request, reply) {
-        reply(`hello ${request.auth.credentials.name}`);
+        reply.file('teacher.html');
       }
     }
   });
